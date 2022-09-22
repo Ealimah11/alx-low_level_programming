519de10dc87cd3e06e24d0bfafc6c39853e77446
Ealimah11 <112158775+Ealimah11@users.noreply.github.com>
2022-09-22 16:06:31 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,45 +1,23 @@
 #include <stdio.h>
 
-
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success/correct)
- *
- *     */
-
+ * Return: Always 0 (Success/correct)
+ */
 int main(void)
-
 {
+	char i;
+	char j;
 
-		char i;
-
-			char j;
-
-
-
-				for (i = 'a'; i <= 'z'; i++)
-
-						{
-
-									putchar(i);
-
-										}
-
-					for (j = 'A'; j <= 'Z'; j++)
-
-							{
-
-										putchar(j);
-
-											}
-
-						putchar('\n');
-
-							return (0);
-
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	for (j = 'A'; j <= 'Z'; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
 }
